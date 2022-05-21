6c4d4011cc3fe650fafc346d240a8fa24e46b783
hugo-toumi <hugo.toumi@laplateforme.io>
2022-05-21 23:41:14 +0200

    tdl fini

--- index.php
+++ index.php
@@ -214,6 +214,17 @@ if(!empty($_POST['login']) && !empty($_POST['mdp'])){
 		</div>
 	</div>
 </body>
+<footer>
+
+        <section class="part-two">
+            <article class="social">
+                <ul>
+                    <a href="https://github.com/hugo-toumi"><img src="https://img.icons8.com/ios-filled/50/000000/github.png"/></a>
+                    <a href="https://hugo-toumi.students-laplateforme.io/"><img src="https://img.icons8.com/ios-filled/50/000000/user-male-circle.png"/></a>
+                    <a href="https://www.linkedin.com/in/hugo-magnani-13b91b1b5/"><img src="https://img.icons8.com/ios-filled/50/000000/linkedin-circled--v1.png"/></a>
+                </ul>
+            </article>
+ </footer>                   
 </html>    
 </body>
 </html>
